0a9ac4117bb9d13a5825c4df0c99aaa5d3a4a834
Atinuke Oluwabamikemi Kayode <64528871+iambami@users.noreply.github.com>
2021-07-22 10:25:58 +0100

    Update calendar_display.py

--- calendar_display.py
+++ calendar_display.py
@@ -6,11 +6,5 @@ import calendar
 yy = 2021  # year
 mm = 7   # month
 
-
-# To take month and year input from the user
-# yy = int(input("Enter year: "))
-# mm = int(input("Enter month: "))
-
-
 # display the calendar
 print(calendar.month(yy, mm))
